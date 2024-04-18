1fbbe97066477bb2f4b608814c8f82e420a2f5e1
iy_r <iyengwork@gmail.com>
2024-04-17 21:12:21 -0500

    day 13 work class. learn about String methods

---
+++ src/day13_recap_string_class/UpperandLower.java
@@ -0,0 +1,14 @@
+package day13_recap_string_class;
+
+public class UpperandLower {
+    public static void main(String[] args) {
+    String str = "sNoW";
+
+        System.out.println(str.toUpperCase());
+
+        str = str.toUpperCase();
+        System.out.println(str);
+
+        System.out.println(str.toLowerCase());
+    }
+}
