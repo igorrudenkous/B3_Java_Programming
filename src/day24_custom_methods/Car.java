324c9b1e60ae2d8dd14261bbad0de72542c79bb0
iy_r <iyengwork@gmail.com>
2024-05-11 14:10:53 -0500

    multi arrays

---
+++ src/day24_custom_methods/Car.java
@@ -0,0 +1,37 @@
+package day24_custom_methods;
+
+public class Car {
+
+    public static void unlockCar () {
+        System.out.println("Unlocking car");
+    }
+
+    public static void OpenDorCar () {
+        System.out.println("Open Door Car");
+    }
+
+    public static void sitInCar () {
+        System.out.println("Sit in a car");
+    }
+
+    public static void closeDoor () {
+        System.out.println("Close Door");
+    }
+
+    public static void puttheseatbelt () {
+        System.out.println("put the seatbelt");
+    }
+
+    public static void startTheEngineRun () {
+        System.out.println("Start The Engine and Run");
+    }
+
+    public static void main(String[] args) {
+        unlockCar();
+        OpenDorCar();
+        sitInCar();
+        closeDoor();
+        puttheseatbelt();
+        startTheEngineRun();
+    }
+}
